aece31a2836796f1398ce54f56c49531b21e53ef
Daniel Wigton <daniel@stonecottageweb.com>
2012-09-16 16:37:48 -0500

    removed unnessary entry from .gitignore

--- modules/test/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Output_Formatter_Module_Config
-{
-    public function config()
-    {
-        Lightning_Event::addObserver('Render_Complete', 'modules/output_formatter/models/observer.php', 'Output_Formatter_Observer', 'formatOutput');
-    }
-}
