42f1f527cd9aa52dbf9a384b2a8e0d6058239be8
daniel <daniel@daniel-Alienware-M17>
2013-10-19 14:00:30 -0500

    cleaned up url and uri logic to work seamlessly when in  a sub directory and allow for url arguments byt stripping them out of the uri.

--- index.php
+++ index.php
@@ -1,27 +1,15 @@
 <?php
-/*
- *  Turn on error reporting for development.
- */
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // Include the App class.
 require_once 'lightning/app.php';
 // Adding a function to find all the base lightnining files.
 spl_autoload_register('loadLightning');
 
-// Set PROJECT_DIR to the part of the URI that points to this Lightning application
-// in the case of www.example.com/ set PROJECT_DIR = "/";
-// in the case where the app resides at www.example.com/somefolder/
-// then set PROJECT_DIR = "/somefolder/";
-
-define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
-define('PROJECT_DIR', '/');
+define('ROOT_PATH', substr($_SERVER['SCRIPT_FILENAME'], 0, -10));
+define('PROJECT_DIR', '/'.trim(strstr($_SERVER['PHP_SELF'], 'index.php', true), '/'));
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].PROJECT_DIR);
 
 // Trim 'REQUEST_URI' if index.php is not in the server's document root.
-define('URI', substr($_SERVER['REQUEST_URI'], strlen(PROJECT_DIR) - 1));
+define('URI', current(explode('?', substr($_SERVER['REQUEST_URI'],strlen(PROJECT_DIR)), 2)));
 
 App::initRouter(URI);
 
